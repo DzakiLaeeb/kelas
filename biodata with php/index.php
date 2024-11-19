fecf7257e8f54c11c996816e197debc5fd239019
arsyad aditya <parsyad86@gmail.com>
2024-11-19 20:37:18 +0700

    Add files via upload

---
+++ biodata with php/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about me</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="hero">
+        <div class="container">
+            <div class="intro">
+                <h1>Hi! I’m arsyad aditya</h1>
+                <p class="subtitle">I am a football player</p>
+                <div class="buttons">
+                    <button>Read More</button>
+                    <button>MY Club</button>
+                </div>
+            </div>
+            <div class="profile">
+                <img src="assets/1.jpg" alt="Profile Picture">
+            </div>
+        </div>
+    </div>
+
+    <div class="about">
+        <div class="container">
+            <div class="skills">
+                <h2>My Skills</h2>
+                <p>defending: 90%</p>
+                <div class="progress-bar">
+                    <div class="progress" style="width: 90%;"></div>
+                </div>
+                <p>dribling: 85%</p>
+                <div class="progress-bar">
+                    <div class="progress" style="width: 85%;"></div>
+                </div>
+                <p>finishing: 75%</p>
+                <div class="progress-bar">
+                    <div class="progress" style="width: 75%;"></div>
+                </div>
+            </div>
+            <div class="about-me">
+                <p>
+                    <h1>Arsyad Aditya Putra</h1>
+                    <h2>Arsyad</h2>
+                   <p>Sidoarjo,10-10-10</p> 
+                   <p>SMKN 2 Buduran</p>
+                   <p>DS Kedungturi,Taman,Sidoarjo</p>
+                   <p>164cm</p>
+                   <p>64kg</p>
+                   <p>football</p>
+
+                    
+                </p>
+                <button>My Portfolio</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
